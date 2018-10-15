10d90af14ba36173670fe25d46b95206b41939d9
Swananda-Punde <swananda-punde@github.com>
2018-10-15 21:11:46 +0530

    Client

---
+++ client/client.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+#include<boost/asio.hpp>
+#include "../Headers/chat_proto.h"
+#include<boost/archive/text_oarchive.hpp>
+#include<boost/archive/text_iarchive.hpp>
+#include<sstream>
+#include<string>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+using boost::asio::ip::udp;
+using namespace boost::archive;
+
+int main(int argc, char *argv[])
+{
+	if(argc < 3)
+	{
+		cout << "Required arguments : ./clent <ip address> <port>" << endl;
+		return 0;
+	}
+	try
+	{
+		boost::asio::io_service io_service;
+		udp::resolver resolver(io_service);
+
+		udp::endpoint receiver_endpoint = *resolver.resolve(udp::v4(), argv[1], argv[2]).begin();
+
+		udp::socket socket(io_service);
+		socket.open(udp::v4());
+
+		chat_proto temp;
+
+		for(int i= 0; ;i++)
+		{
+			char buffer[100];
+			
+			cout << "Enter Message To send : ";
+			cin >> buffer;
+			if(std::to_string(buffer)."exit")
+				break;
+			socket.send_to(boost::asio::buffer(buffer), receiver_endpoint);
+
+			udp::endpoint sender_endpoint;
+			size_t len = socket.receive_from(boost::asio::buffer(buffer), sender_endpoint);
+			
+			std::istringstream ss(buffer);
+			text_iarchive ia(ss);
+			ia >> temp;
+		
+			cout<< "\nReceived from : "<<temp.get_from()<< " : "<<temp.get_msg()<<"\nSize : "<<len<<endl;
+		}
+	}
+	catch(std::exception &e) 
+	{
+		cout<<"\nError Occuured \n"<<e.what()<<endl;
+	}
+}
